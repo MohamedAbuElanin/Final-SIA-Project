a3f476b37447b9f0f505ed3535239c688fec2d1b
MohamedAbuElanin <medsmohamed910@gmail.com>
2025-12-08 01:14:55 +0200

    V1.009

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -4,9 +4,9 @@
 // Do not commit service account keys (private keys) which are for backend only.
 const firebaseConfig = {
   apiKey: "AIzaSyBbcUi02rCzwZOVY3uRloGKk21-fC7IFDk",
-  authDomain: "sia-project-2458a.firebaseapp.com",
-  projectId: "sia-project-2458a",
-  storageBucket: "sia-project-2458a.firebasestorage.app",
+  authDomain: "sia-993a7.firebaseapp.com",
+  projectId: "sia-993a7",
+  storageBucket: "sia-993a7.firebasestorage.app",
   messagingSenderId: "415064406442",
   appId: "1:415064406442:web:c1550b2aad4d331b8b53d3",
   measurementId: "G-4F3VZ10MX1",
